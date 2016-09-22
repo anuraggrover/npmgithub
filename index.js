56ce9275ce3cb455820add42bac6bd10f2f4a53c
rogue87 <grover.anurag87@gmail.com>
2016-09-22 14:42:27 +0530

    add module

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by anurag on 22/09/16.
+ */
+
+module.exports = function (a, b) {
+  return a + b;
+};
---
+++ test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by anurag on 22/09/16.
+ */
+
+const add = require('./index');
+
+console.log('added', add(2, 3));
